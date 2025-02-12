ef6adcc1eaf0e7c035ea8a3c8c99f85ca35b993d
Lizord <mt.demakov@gmail.com>
2025-02-12 12:19:59 +0300

    atomic

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <atomic>
 #include <iostream>
 #include <mutex>
 #include <thread>
@@ -38,6 +39,20 @@ void decrease(bool should_lock) {
     }
 }
 
+std::atomic<int> counter_atomic = 0;
+
+void increase_atomic() {
+    for(int i = 0; i < 10000; i++) {
+        counter_atomic.fetch_add(1);
+    }
+}
+
+void decrease_atomic() {
+    for(int i = 0; i < 10000; i++) {
+        counter_atomic.fetch_add(-1);
+    }
+}
+
 int main() {
     std::thread inc(increase, false);
     std::thread dec(decrease, false);
@@ -49,13 +64,29 @@ int main() {
 
     counter = 0;
 
+    PERF_START(mutex);
+
     std::thread inc_lock(increase, true);
     std::thread dec_lock(decrease, true);
 
     inc_lock.join();
     dec_lock.join();
 
-    std::cout << "LOCK: " << counter << std::endl;
+    PERF_END(mutex);
+
+    std::cout << "LOCK: " << counter << " " << PERF_RESULT(mutex) << "mcs" << std::endl;
+
+    PERF_START(atomic);
+
+    std::thread inc_atomic(increase_atomic);
+    std::thread dec_atomic(decrease_atomic);
+
+    inc_atomic.join();
+    dec_atomic.join();
+
+    PERF_END(atomic);
+
+    std::cout << "ATOMIC: " << counter_atomic << " " << PERF_RESULT(atomic) << "mcs" << std::endl;
     
     return 0;
 }
